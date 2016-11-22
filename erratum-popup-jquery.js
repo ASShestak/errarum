17c1f2cce0adc2b87fe4ce9a1d3f636e7e2205d1
Alex Shestak <iskender1990@yandex.ru>
2016-11-22 11:58:04 +0300

    Added erratum-popup js with jquery.

---
+++ erratum-popup-jquery.js
@@ -0,0 +1,127 @@
+(function ($) {
+  $(document).ready(function() {
+    var erratum = {
+      fields: Drupal.settings.erratum,
+      popup: $('#erratum-overlay'),
+
+      init: function() {
+        var th = this;
+        $(document).bind('keypress', function(e) {
+          th.onKeyPress(e);
+        });
+        this.popup.find('#erratum-overlay-close').bind('click', function(e) {
+          th.closePopup();
+        });
+      },
+      closePopup: function() {
+        this.popup.hide();
+        this.popup.find('#erratum-json').val('');
+        this.popup.find('#erratum-message').remove();
+        this.popup.find('#erratum-popup-form').show();
+      },
+      updatePopup: function(data) {
+        if (typeof this.fields[data.key] !== 'undefined') {
+          $.extend(data, this.fields[data.key]);
+          if (data.fragment && data.fragment.length < data.max_length) {
+            var json = {
+              entity_type: data.entity_type,
+              entity_id: data.entity_id,
+              field_name: data.field_name,
+              fragment: data.fragment,
+              mail: data.mail
+            }
+
+            this.popup.find('#erratum-selected-fragment').html(data.pre + '<span>' + data.fragment + '</span>' + data.suf);
+            this.popup.find('#erratum-popup-text').html(data.popup_text);
+            this.popup.find('#erratum-json').val(JSON.stringify(json));
+            this.popup.show();
+          }
+        }
+      },
+      getSelectedData: function() {
+        var textRange = null,
+            selectedText = null;
+
+        if (window.getSelection) {
+          selectedText = window.getSelection();
+        } else {
+          selectedText = (window.document.getSelection) ? window.document.getSelection() : window.document.selection;
+        }
+
+        if (selectedText != null) {
+          var pre = '',
+              suf = '',
+              range = null,
+              newRange = null;
+          // Get prefix and suffix.
+          if (selectedText.getRangeAt) {
+            range = selectedText.getRangeAt(0);
+            textRange = range.toString();
+            newRange = window.document.createRange();
+            newRange.setStartBefore(range.startContainer.ownerDocument.body);
+            newRange.setEnd(range.startContainer, range.startOffset);
+            pre = newRange.toString();
+            newRange = range.cloneRange();
+            newRange.setStart(range.endContainer, range.endOffset);
+            newRange.setEndAfter(range.endContainer.ownerDocument.body);
+            suf = newRange.toString();
+          } else {
+            if (selectedText.createRange) {
+              range = selectedText.createRange();
+              textRange = range.text;
+              newRange = selectedText.createRange();
+              newRange.moveStart("character", -60);
+              newRange.moveEnd("character", -textRange.length);
+              pre = newRange.text;
+              newRange = selectedText.createRange();
+              newRange.moveEnd("character", 60);
+              newRange.moveStart("character", textRange.length);
+              suf = newRange.text;
+            } else {
+              textRange = "" + selectedText;
+            }
+          }
+          // Remove whitespaces.
+          var p;
+          var s = (p = textRange.match(/^(\s*)/)) && p[0].length;
+          var e = (p = textRange.match(/(\s*)$/)) && p[0].length;
+          pre = pre + textRange.substring(0, s);
+          suf = textRange.substring(textRange.length - e, textRange.length) + suf;
+          textRange = textRange.substring(s, textRange.length - e);
+        } else {
+          return;
+        }
+
+        var parent = $(selectedText.anchorNode).closest('.erratum-use');
+        if (textRange == '' || parent.length == 0) {
+          return;
+        }
+
+        return {
+          pre: pre.substring(pre.length - 60, pre.length).replace(/^\S{1,10}\s+/, '').replace(/\s+/g, ' '),
+          fragment: textRange.replace(/\s+/g, ' '),
+          suf: suf.substring(0, 60).replace(/\s+\S{1,10}$/, '').replace(/\s+/g, ' '),
+          key: parent.attr('field-key'),
+          entity_id: parent.attr('field-entity-id')
+        }
+      },
+      onKeyPress: function(e) {
+        var ctrlEnt = 0,
+            we = window.event;
+        if (we) {
+          ctrlEnt = we.keyCode == 10 || (we.keyCode == 13 && we.ctrlKey);
+        } else if (e) {
+          ctrlEnt = (e.which == 10 && e.modifiers == 2) || (e.keyCode == 0 && e.charCode == 106 && e.ctrlKey) || (e.keyCode == 13 && e.ctrlKey);
+        }
+        if (ctrlEnt) {
+          var selectedData = this.getSelectedData();
+          if (selectedData) {
+            this.updatePopup(selectedData);
+          }
+        }
+      }
+    }
+
+    erratum.init();
+  });
+})(jQuery);
